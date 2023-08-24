0197c3f4e58a65bb8fb400f0583616d36db22cbd
shahil9523 <shahil.s@zebpay.com>
2023-08-24 17:51:02 +0530

    photos api added

--- app/src/main/java/com/example/applicationdemo/codingsk/ktorclientandroid/data/remote/dto/HttpRoutes.kt
+++ app/src/main/java/com/example/applicationdemo/codingsk/ktorclientandroid/data/remote/dto/HttpRoutes.kt
@@ -1,6 +1,6 @@
 package com.example.applicationdemo.codingsk.ktorclientandroid.data.remote.dto
 
 object HttpRoutes {
-    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
-    const val POSTS = "$BASE_URL/posts"
+    private const val BASE_URL = "https://api.unsplash.com/"
+    const val POSTS = "$BASE_URL/photos"
 }
